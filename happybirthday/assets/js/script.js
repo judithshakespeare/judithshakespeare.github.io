4bf3af56ee884aaa689d2dc2c51f1068c5ff9769
judithshakespeare <52847756+judithshakespeare@users.noreply.github.com>
2019-07-26 20:21:02 +0900

    Update script.js

--- happybirthday/assets/js/script.js
+++ happybirthday/assets/js/script.js
@@ -19,12 +19,13 @@ $(document).ready(function(){
             if(count==1){
             document.querySelector('#items').innerHTML = randomItem(items);
             function randomItem(a){return a[Math.floor(Math.random() * a.length)];};
+        }else if(count==2){
             setTimeout(function(){
                 // $("#items").fadeOut(1200);
-                $("#envelope").fadeIn(1500);},1200);
+            $("#envelope").fadeIn(1500);},1200);
             $("#paperbag").hide();
             $("#alert").hide();
-        }else if(count==2){
+        }else if(count==3){
             $("#envelopeOpen").fadeIn(900);
             setTimeout(function(){
                 $("#paper").fadeIn(1000);
