b33f670b2c3704cd81749d3ee9dfe3a643c929ae
judithshakespeare <52847756+judithshakespeare@users.noreply.github.com>
2019-11-26 11:55:05 +0900

    Update script.js

--- whynotus/assets/js/script.js
+++ whynotus/assets/js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  // var mql = window.matchMedia("screen and (max-width: 768px)");
-  // if (mql.matches) {
     $('.backgrounds').first().addClass('active');
     $('.backgrounds').hide();    
     $('.active').show();
@@ -33,7 +31,8 @@ $(document).ready(function(){
         'width' : '110px',
         'position' : 'absolute',
         'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
-        'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
+        'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2),
+        'z-index' : '6'
       });
       $('#diary-wrapper').append(clone);
       $(function(e){
@@ -48,11 +47,11 @@ $(document).ready(function(){
         });
       });
       $(".stickerdraggable").on('mousedown',function(){
-        $('#diary-wrapper').children('img').not(this).css('z-index','5');
         $(this).css({
           'width' : "120px",
           'z-index' : '6'
         });
+        $('#diary-wrapper').children('img').not(this).css('z-index','5');
       });
       $('#trash').droppable({
         over: function(event, ui) {
@@ -60,9 +59,41 @@ $(document).ready(function(){
         }
       });
     });
+    $('.gemSticker').click(function(e){
+      var clone = $(this).clone().attr('class','gemStickerdraggable').css({
+        'width' : '100px',
+        'position' : 'absolute',
+        'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
+        'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
+      });
+      $('#diary-wrapper').append(clone);
+      $(function(e){
+        $(".gemStickerdraggable").draggable({
+          containment: 'parent',
+          scroll: false
+        });
+      });
+      $(".gemStickerdraggable").on('mouseup',function(e){
+        $(this).css({
+          width: "100px"
+        });
+      });
+      $(".gemStickerdraggable").on('mousedown',function(){
+        $(this).css({
+          'width' : "110px",
+          'z-index' : '6'
+        });
+        $('#diary-wrapper').children('img').not(this).css('z-index','5');
+      });
+      $('#trash').droppable({
+        over: function(event, ui){
+          ui.draggable.remove();
+        }
+      });
+    });
     $('.imgSticker').click(function(e){
       var clone = $(this).clone().attr('class','imgStickerdraggable').css({
-        'width' : '170px',
+        'width' : '25%',
         'position' : 'absolute',
         'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
         'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
@@ -76,16 +107,15 @@ $(document).ready(function(){
       });
       $(".imgStickerdraggable").on('mouseup',function(e){
         $(this).css({
-          width: "170px"
+          width: "25%"
         });
       });
       $(".imgStickerdraggable").on('mousedown',function(){
-        $(this).css('z-index','6');
-        $('#diary-wrapper').children('img').not(this).css('z-index','5');
         $(this).css({
-          'width' : "180px",
+          'width' : "27%",
           'z-index' : '6'
         });
+        $('#diary-wrapper').children('img').not(this).css('z-index','5');
       });
       $('#trash').droppable({
         over: function(event, ui){
@@ -95,11 +125,10 @@ $(document).ready(function(){
     });
     $('.wordSticker').click(function(e){
       var clone = $(this).clone().attr('class','wordStickerdraggable').css({
-        'width' : '200px',
+        'width' : '35%',
         'position' : 'absolute',
         'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
         'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2),
-        'z-index' : '6'
       });
       $('#diary-wrapper').append(clone);
       $(function(e){
@@ -110,16 +139,15 @@ $(document).ready(function(){
       });
       $(".wordStickerdraggable").on('mouseup',function(e){
         $(this).css({
-          width: "200px"
+          width: "35%"
         });
       });
       $(".wordStickerdraggable").on('mousedown',function(){
-        $(this).css('z-index','6');
-        $('#diary-wrapper').children('img').not(this).css('z-index','5');
         $(this).css({
-          'width' : "210px",
+          'width' : "36%",
           'z-index' : '6'
         });
+        $('#diary-wrapper').children('img').not(this).css('z-index','5');
       });
       $('#trash').droppable({
         over: function(event, ui){
@@ -147,12 +175,11 @@ $(document).ready(function(){
         });
       });
       $(".miniStickerdraggable").on('mousedown',function(){
-        $(this).css('z-index','6');
-        $('#diary-wrapper').children('img').not(this).css('z-index','5');
         $(this).css({
           'width' : "2.1em",
           'z-index' : '6'
         });
+        $('#diary-wrapper').children('img').not(this).css('z-index','5');
       });
       $('#trash').droppable({
         over: function(event, ui){
@@ -160,170 +187,4 @@ $(document).ready(function(){
         }
       });
     });
-////////////////////
-  // } else {
-  //   $('.backgrounds').first().addClass('active');
-  //   $('.backgrounds').hide();    
-  //   $('.active').show();
-  //   $('#nextBtn').click(function(){
-  //     $('.active').removeClass('active').addClass('oldActive');    
-  //     if ( $('.oldActive').is(':last-child')) {
-  //       $('.backgrounds').first().addClass('active');
-  //     }
-  //     else{
-  //       $('.oldActive').next().addClass('active');
-  //     }
-  //     $('.oldActive').removeClass('oldActive');
-  //     $('.backgrounds').fadeOut();
-  //     $('.active').fadeIn();
-  //   });
-  //   $('#backBtn').click(function(){
-  //     $('.active').removeClass('active').addClass('oldActive');    
-  //     if ( $('.oldActive').is(':first-child')) {
-  //       $('.backgrounds').last().addClass('active');
-  //     }
-  //     else{
-  //       $('.oldActive').prev().addClass('active');
-  //     }
-  //     $('.oldActive').removeClass('oldActive');
-  //     $('.backgrounds').fadeOut();
-  //     $('.active').fadeIn();
-  //   });
-  //   $('.sticker').click(function(e){
-  //     var clone = $(this).clone().attr('class','stickerdraggable').css({
-  //       'width' : '15%',
-  //       'position' : 'absolute',
-  //       'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
-  //       'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
-  //     });
-  //     $('#diary-wrapper').append(clone);
-  //     $(function(e){
-  //       $(".stickerdraggable").draggable({
-  //         containment: 'parent',
-  //         scroll: false
-  //       });
-  //     });
-  //     $(".stickerdraggable").on('mouseup',function(e){
-  //       $(this).css({
-  //         width: "15%"
-  //       });
-  //     });
-  //     $(".stickerdraggable").on('mousedown',function(){
-  //       $(this).css('z-index','6');
-  //       $('#diary-wrapper').children('img').not(this).css('z-index','5');
-  //       $(this).css({
-  //         'width' : "16%",
-  //         'z-index' : '6'
-  //       });
-  //     });
-  //     $('#trash').droppable({
-  //       over: function(event, ui) {
-  //         ui.draggable.remove();
-  //       }
-  //     });
-  //     $(".stickerdraggable").on("click",function(){
-
-  //     });
-  //   });
-  //   $('.imgSticker').click(function(e){
-  //     var clone = $(this).clone().attr('class','imgStickerdraggable').css({
-  //       'width' : '170px',
-  //       'position' : 'absolute',
-  //       'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
-  //       'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
-  //     });
-  //     $('#diary-wrapper').append(clone);
-  //     $(function(e){
-  //       $(".imgStickerdraggable").draggable({
-  //         containment: 'parent',
-  //         scroll: false
-  //       });
-  //     });
-  //     $(".imgStickerdraggable").on('mouseup',function(e){
-  //       $(this).css({
-  //         width: "170px"
-  //       });
-  //     });
-  //     $(".imgStickerdraggable").on('mousedown',function(){
-  //       $(this).css('z-index','6');
-  //       $('#diary-wrapper').children('img').not(this).css('z-index','5');
-  //       $(this).css({
-  //         'width' : "180px",
-  //         'z-index' : '6'
-  //       });
-  //     });
-  //     $('#trash').droppable({
-  //       over: function(event, ui){
-  //         ui.draggable.remove();
-  //       }
-  //     });
-  //   });
-  //   $('.wordSticker').click(function(e){
-  //     var clone = $(this).clone().attr('class','wordStickerdraggable').css({
-  //       'width' : '300px',
-  //       'position' : 'absolute',
-  //       'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
-  //       'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2),
-  //       'z-index' : '6'
-  //     });
-  //     $('#diary-wrapper').append(clone);
-  //     $(function(e){
-  //       $(".wordStickerdraggable").draggable({
-  //         containment: 'parent',
-  //         scroll: false
-  //       });
-  //     });
-  //     $(".wordStickerdraggable").on('mouseup',function(e){
-  //       $(this).css({
-  //         width: "300px"
-  //       });
-  //     });
-  //     $(".wordStickerdraggable").on('mousedown',function(){
-  //       $(this).css('z-index','6');
-  //       $('#diary-wrapper').children('img').not(this).css('z-index','5');
-  //       $(this).css({
-  //         'width' : "310px",
-  //         'z-index' : '6'
-  //       });
-  //     });
-  //     $('#trash').droppable({
-  //       over: function(event, ui){
-  //         ui.draggable.remove();
-  //       }
-  //     });
-  //   });
-  //   $('.miniSticker').click(function(e){
-  //     var clone = $(this).clone().attr('class','miniStickerdraggable').css({
-  //       'width' : '40px',
-  //       'position' : 'absolute',
-  //       'top' : Math.random() * ($('#diary-wrapper').height() - $(this).height() * 1.8),
-  //       'left' : Math.random() * ($('#diary-wrapper').width() - $(this).width() * 2)
-  //     });
-  //     $('#diary-wrapper').append(clone);
-  //     $(function(e){
-  //       $(".miniStickerdraggable").draggable({
-  //         containment: 'parent',
-  //         scroll: false
-  //       });
-  //     });
-  //     $(".miniStickerdraggable").on('mouseup',function(e){
-  //       $(this).css({
-  //         width: "40px"
-  //       });
-  //     });
-  //     $(".miniStickerdraggable").on('mousedown',function(){
-  //       $(this).css('z-index','6');
-  //       $('#diary-wrapper').children('img').not(this).css('z-index','5');
-  //       $(this).css({
-  //         'width' : "45px",
-  //         'z-index' : '6'
-  //       });
-  //     });
-  //     $('#trash').droppable({
-  //       over: function(event, ui){
-  //         ui.draggable.remove();
-  //       }
-  //     });
-  //   });
-  // }
 });
